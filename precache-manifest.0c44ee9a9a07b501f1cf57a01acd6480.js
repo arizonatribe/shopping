0884356d1d88f68449a5a8bbb3bd233c674da30c
David Nunez <arizonatribe@gmail.com>
2020-02-12 09:33:43 -0700

    Updates

---
+++ precache-manifest.0c44ee9a9a07b501f1cf57a01acd6480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a5b59d439a57471eddc25f2c1f0df4",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "d6672d4e3a1bf76553fb",
+    "url": "/shopping/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "f59f6704c334927862b3",
+    "url": "/shopping/static/js/2.243f5bc8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shopping/static/js/2.243f5bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6672d4e3a1bf76553fb",
+    "url": "/shopping/static/js/main.9d9fda46.chunk.js"
+  },
+  {
+    "revision": "03945516021b0d4e1a84",
+    "url": "/shopping/static/js/runtime-main.5e26cc9e.js"
+  }
+]);
