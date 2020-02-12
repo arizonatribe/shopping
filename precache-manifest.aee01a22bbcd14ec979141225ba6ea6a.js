9314f7bc520e286672101db471a3fcaadd5d8795
David Nunez <arizonatribe@gmail.com>
2020-02-12 10:06:33 -0700

    Updates

---
+++ precache-manifest.aee01a22bbcd14ec979141225ba6ea6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bd77bac26adab3716b003858375789",
+    "url": "https://arizonatribe.github.io/shopping/index.html"
+  },
+  {
+    "revision": "888dca6fcbac26358980",
+    "url": "https://arizonatribe.github.io/shopping/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "02d054e292962748d607",
+    "url": "https://arizonatribe.github.io/shopping/static/js/2.d192706b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "https://arizonatribe.github.io/shopping/static/js/2.d192706b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888dca6fcbac26358980",
+    "url": "https://arizonatribe.github.io/shopping/static/js/main.f7f66053.chunk.js"
+  },
+  {
+    "revision": "226b8e23f02708759da7",
+    "url": "https://arizonatribe.github.io/shopping/static/js/runtime-main.b033bc6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shopping/precache-manifest.d20664d4f54ce918e00f07e3745ca9e5.js"
+  "https://arizonatribe.github.io/shopping/precache-manifest.aee01a22bbcd14ec979141225ba6ea6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shopping/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arizonatribe.github.io/shopping/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
